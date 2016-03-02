a7d2d4ce6964befccb2d825d85d6eeab9f2e35b7
rsgc-leder-b <bleder@rsgc.on.ca>
2016-03-02 09:57:13 -0500

    Terms of Office

---
+++ Terms of Office/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Terms of Office
+//
+//  Created by Brendan Leder on 2016-03-02.
+//  Copyright © 2016 Brendan Leder. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+var futureYear : Double?
+var curentYear : Double?
+
+print("Enter the current year:")
